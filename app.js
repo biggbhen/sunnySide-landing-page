7cdf6cdc5a68a6778b4eba64e8d9f4954bb3685f
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-27 17:05:00 +0100

    applied font awesome

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const ham = document.querySelector('.harmburger')
 ham.addEventListener('click', x =>{
 document.querySelector('.hamburgerMenu').style.display='block'
+ham.style.width = '2.8125rem'
+ham.setAttribute('src', src="images/cancel.png")
 })
