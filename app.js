5812071e6497dcdf570e4ccafb056128942f893c
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-29 18:11:50 +0100

    finished the harmburgermenu

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const ham = document.querySelector('.hamburger')
 // console.log(ham);
-ham.addEventListener('click', (x) =>{
+ham.addEventListener('click', x =>{
+if (ham.classList.contains('fa-bars')) {
  ham.classList.replace("fa-bars", 'fa-xmark')
-document.querySelector('.hamburgerMenu').style.display='block'
-console.log(x.target);
-
+ document.querySelector('.hamburgerMenu').style.display = 'block';
+}
+else{
+ document.querySelector('.hamburgerMenu').style.display = 'none';
+  ham.classList.replace('fa-xmark', "fa-bars")
+}
 })
