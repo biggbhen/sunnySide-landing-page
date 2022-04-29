6b835cf3af82c2288f0abc6f69c0b4c8687f517d
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-29 17:15:23 +0100

    worked on some animation props

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// const ham = document.querySelector('.hamburger')
-// // console.log(ham);
-// ham.addEventListener('click', (x) =>{
-// //  ham.classList.replace("fa-bars", 'fa-xmark')
-// // document.querySelector('.hamburgerMenu').style.display='block'
-// // console.log(x.target);
+const ham = document.querySelector('.hamburger')
+// console.log(ham);
+ham.addEventListener('click', (x) =>{
+ ham.classList.replace("fa-bars", 'fa-xmark')
+document.querySelector('.hamburgerMenu').style.display='block'
+console.log(x.target);
 
-// })
+})
