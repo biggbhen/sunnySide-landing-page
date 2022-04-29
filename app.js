924291f2fd4e6bee439b8ecc661502bb448d8587
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-29 18:23:46 +0100

    done for now

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const ham = document.querySelector('.hamburger')
+const hamburgerMenu = document.querySelector('.hamburgerMenu')
 // console.log(ham);
 ham.addEventListener('click', x =>{
 if (ham.classList.contains('fa-bars')) {
  ham.classList.replace("fa-bars", 'fa-xmark')
- document.querySelector('.hamburgerMenu').style.display = 'block';
+ hamburgerMenu.style.display = 'block';
+ // hamburgerMenu.style.transition = 'all 0.3s ease-in-out';
 }
 else{
- document.querySelector('.hamburgerMenu').style.display = 'none';
+ hamburgerMenu.style.display = 'none';
   ham.classList.replace('fa-xmark', "fa-bars")
 }
 })
+
