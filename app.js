7aed031c6c4ccfef5802f4a7d476a5846186d946
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-02 15:23:50 +0100

    donee

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const ham = document.querySelector('.hamburger')
-const hamburgerMenu = document.querySelector('.hamburgerMenu')
+const ham = document.querySelector('.hamburger');
+const hamburgerMenu = document.querySelector('.hamburgerMenu');
 // console.log(ham);
-ham.addEventListener('click', x =>{
-if (ham.classList.contains('fa-bars')) {
- ham.classList.replace("fa-bars", 'fa-xmark')
- hamburgerMenu.style.display = 'block';
- // hamburgerMenu.style.transition = 'all 0.3s ease-in-out';
-}
-else{
- hamburgerMenu.style.display = 'none';
-  ham.classList.replace('fa-xmark', "fa-bars")
-}
-})
-
+ham.addEventListener('click', (x) => {
+  if (ham.classList.contains('fa-bars')) {
+    ham.classList.replace('fa-bars', 'fa-xmark');
+    hamburgerMenu.style.display = 'block';
+    hamburgerMenu.style.transition = 'all 0.3s ease-in-out';
+  } else {
+    hamburgerMenu.style.display = 'none';
+    ham.classList.replace('fa-xmark', 'fa-bars');
+  }
+});
