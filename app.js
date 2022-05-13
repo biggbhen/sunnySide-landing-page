f284029ce94c17b240c6c80cee9181c7f534ced9
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-13 23:38:55 +0100

    remodelling

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const hamburgerMenu = document.querySelector('.hamburgerMenu'),
-  ham = document.querySelector('.hamburger'),
-  removeIcon = document.querySelector('.cancel');
-function showHamburger() {
-  ham.style.display = 'none';
-  hamburgerMenu.style.display = 'block';
-  removeIcon.style.display = 'block';
-}
-function closeHamburger() {
-  ham.style.display = 'block';
-  hamburgerMenu.style.display = 'none';
-  removeIcon.style.display = 'block';
-}
-// console.log(ham);
-ham.addEventListener('click', (e) => {
-  if () {
-    showHamburger();
-  } else {
-    closeHamburger();
-  }
-  // console.log(ham);
-});
+// const hamburgerMenu = document.querySelector('.hamburgerMenu'),
+//   ham = document.querySelector('.hamburger'),
+//   removeIcon = document.querySelector('.cancel'),
+//   navbar = document.querySelector('nav');
+// function showHamburger() {
+//   ham.setAttribute('src', './images/x-10329.svg');
+//   hamburgerMenu.style.display = 'block';
+//   ham.classList.add('cancel');
+//   console.log(ham);
+// }
+// function closeHamburger() {
+//   ham.style.display = 'block';
+//   hamburgerMenu.style.display = 'none';
+//   ham.setAttribute('src', './images/icon-hamburger.svg');
+// }
+// // console.log(ham);
+// navbar.addEventListener('click', (e) => {
+//   if (e.target === ham) {
+//     showHamburger();
+//   }
+// });
