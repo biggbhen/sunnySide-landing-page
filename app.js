b7ce1cb45cb9eda91e2a4f36a146d1136b34c5d7
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-14 00:48:52 +0100

    done.. hurray!

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-// const hamburgerMenu = document.querySelector('.hamburgerMenu'),
-//   ham = document.querySelector('.hamburger'),
-//   removeIcon = document.querySelector('.cancel'),
-//   navbar = document.querySelector('nav');
-// function showHamburger() {
-//   ham.setAttribute('src', './images/x-10329.svg');
-//   hamburgerMenu.style.display = 'block';
-//   ham.classList.add('cancel');
-//   console.log(ham);
-// }
-// function closeHamburger() {
-//   ham.style.display = 'block';
-//   hamburgerMenu.style.display = 'none';
-//   ham.setAttribute('src', './images/icon-hamburger.svg');
-// }
-// // console.log(ham);
-// navbar.addEventListener('click', (e) => {
-//   if (e.target === ham) {
-//     showHamburger();
-//   }
-// });
+const hamburgerMenu = document.querySelector('.hamburgerMenu'),
+  hamButton = document.querySelector('.hamburger'),
+  menuIcon = document.querySelector('.menu-icon'),
+  closeIcon = document.querySelector('.close-icon');
+function showHamburger() {
+  hamburgerMenu.classList.remove('showMenu');
+  closeIcon.style.display = 'none';
+  menuIcon.style.display = 'block';
+  hamburgerMenu.style.transform = 'translateY(-150%)';
+}
+function closeHamburger() {
+  hamburgerMenu.classList.add('showMenu');
+  closeIcon.style.display = 'block';
+  menuIcon.style.display = 'none';
+  hamburgerMenu.style.transform = 'translateY(0)';
+}
+hamButton.addEventListener('click', (x) => {
+  if (hamburgerMenu.classList.contains('showMenu')) {
+    showHamburger();
+  } else {
+    closeHamburger();
+  }
+  console.log(hamburgerMenu);
+  x.preventDefault();
+});
