ec3c44c9954a505428ef4e7344dc777e9fa25320
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-13 19:50:42 +0100

    trying to toggle menu

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const ham = document.querySelector('.hamburger');
-const hamburgerMenu = document.querySelector('.hamburgerMenu');
-
+const hamburgerMenu = document.querySelector('.hamburgerMenu'),
+  ham = document.querySelector('.hamburger'),
+  removeIcon = document.querySelector('.cancel');
+function showHamburger() {
+  ham.style.display = 'none';
+  hamburgerMenu.style.display = 'block';
+  removeIcon.style.display = 'block';
+}
+function closeHamburger() {
+  ham.style.display = 'block';
+  hamburgerMenu.style.display = 'none';
+  removeIcon.style.display = 'block';
+}
 // console.log(ham);
-ham.addEventListener('click', (x) => {
-  // ham.classList.toggle('hamburger');
-  console.log(ham);
+ham.addEventListener('click', (e) => {
+  if () {
+    showHamburger();
+  } else {
+    closeHamburger();
+  }
+  // console.log(ham);
 });
