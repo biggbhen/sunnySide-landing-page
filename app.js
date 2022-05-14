ce6834b9985cde2d72daf4543d00d82bd76083e0
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-14 18:46:07 +0100

    updated the click functio to work better

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ function showHamburger() {
   hamburgerMenu.classList.remove('showMenu');
   closeIcon.style.display = 'none';
   menuIcon.style.display = 'block';
-  hamburgerMenu.style.transform = 'translateY(-150%)';
+  hamburgerMenu.style.transform = 'translateX(150%)';
 }
 function closeHamburger() {
   hamburgerMenu.classList.add('showMenu');
   closeIcon.style.display = 'block';
   menuIcon.style.display = 'none';
-  hamburgerMenu.style.transform = 'translateY(0)';
+  hamburgerMenu.style.transform = 'translateX(0%)';
 }
 hamButton.addEventListener('click', (x) => {
   if (hamburgerMenu.classList.contains('showMenu')) {
@@ -20,6 +20,6 @@ hamButton.addEventListener('click', (x) => {
   } else {
     closeHamburger();
   }
-  // console.log(hamburgerMenu);
+
   x.preventDefault();
 });
